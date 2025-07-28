d4b5d64f24ab63bd40f0f26beee9c05311d21d5c
Rahul kumar <12202298+rahul-akumar@users.noreply.github.com>
2025-07-28 07:06:30 +0530

    refactor: update README and improve styles in App.vue and index.html for better clarity and organization

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@ import { Github, Linkedin, Instagram, Twitter, Mail } from "lucide-vue-next";
 
 // Organized utility classes
 const styles = {
-  header: "pl-3 md:pl-12",
+  header: "pl-8 flex flex-col gap-20",
   title:
-    "indent-0 font-medium tracking-tight text-white text-7xl md:text-7xl lg:text-8xl xl:text-[12rem]",
-  main: "p-4 my-4 md:pl-16 md:p-4 md:mt-20 flex flex-col gap-8 md:gap-24 h-full",
-  subtitle: "text-lg text-stone-300 uppercase",
-  sections: "flex flex-row md:flex-row gap-16 md:gap-48",
-  linksList: "flex list-outside list-none flex-col gap-4 md:gap-12",
-  sectionHeader: "tracking-wide text-stone-300 text-lg",
-  workList: "flex flex-col md:flex-row list-none gap-4 md:gap-12",
-  workHeader: "tracking-wide text-stone-300 text-lg mb-0",
-  footer: "pb-16 pl-3.5 md:pl-88 pt-2",
+    "indent-0 font-medium tracking-tighter text-white text-7xl md:text-8xl lg:text-9xl xl:text-[12rem]",
+  subtitle: "px-2 text-md lg:text-lg text-white/70 uppercase tracking-widest",
+  main: "pl-6 mt-20 flex flex-col gap-8 md:gap-24",
+  section: "flex pl-4 gap-20 md:gap-40",
+  sectionList: "flex list-none flex-col gap-10 text-md lg:text-lg",
+  sectionHeader: "tracking-widest text-white/70 text-md lg:text-lg",
+  sectionItem:
+    " text-white underline underline-offset-12 decoration-white/50 hover:decoration-white hover:text-white/80 focus:ring-2 hover:after:content-['_↗'] after:text-amber-500",
+  footer: "py-16 pl-3.5 md:pl-68 pt-2",
   footerGrid: "flex gap-10 md:flex gap-4 md:gap-24",
 };
 </script>
@@ -26,24 +26,21 @@ const styles = {
         href="https://www.dialpad.com/"
         target="_blank"
         rel="noopener noreferrer"
-        class="underline decoration-stone-600 decoration-2 underline-offset-24 focus:ring-4 md:underline-offset-80"
+        class="underline decoration-white/70 decoration-2 underline-offset-24 focus:ring-4 md:underline-offset-80"
         >Dialpad</a
       >.
     </h1>
+    <p :class="styles.subtitle">Systems first • Software designer</p>
   </header>
 
   <main :class="styles.main">
-    <p :class="styles.subtitle">
-      Full stack • Systems first • Software designer
-    </p>
-
-    <div :class="styles.sections">
+    <div :class="styles.section">
       <section>
-        <ul :class="styles.linksList">
+        <ul :class="styles.sectionList">
           <header :class="styles.sectionHeader">IMPORTANT LINKS</header>
           <li>
             <a
-              class="section-link"
+              :class="styles.sectionItem"
               href="https://rahulkumar.notion.site/2343f4f51b468091ba6ad62b3ac7571d?v=2343f4f51b46801cb30e000cc8007f16&source=copy_link"
               target="_blank"
               rel="noopener noreferrer"
@@ -53,7 +50,7 @@ const styles = {
           </li>
           <li>
             <a
-              class="section-link"
+              :class="styles.sectionItem"
               href="https://rahulkumar.notion.site/Rahul-s-resume-17a3f4f51b468043b200eef691b438b5?source=copy_link"
               target="_blank"
               rel="noopener noreferrer"
@@ -63,7 +60,7 @@ const styles = {
           </li>
           <li>
             <a
-              class="section-link"
+              :class="styles.sectionItem"
               href="https://rahulkumar.notion.site/Publication-7b38e95cc3f84f6c8bdbec401996065d?source=copy_link"
               target="_blank"
               rel="noopener noreferrer"
@@ -75,12 +72,12 @@ const styles = {
       </section>
 
       <section>
-        <ul :class="styles.workList">
-          <li :class="styles.workHeader">PAST WORK</li>
+        <ul :class="styles.sectionList" class="flex flex-col sm:flex-row">
+          <li :class="styles.sectionHeader">PAST WORK</li>
           <li>
             <a
-              class="section-link"
-              href="https://github.com/rahul-akumar/REGO"
+              :class="styles.sectionItem"
+              href="https://rahul-akumar.github.io/REGO/"
               target="_blank"
               rel="noopener noreferrer"
               >REGO</a
@@ -88,7 +85,7 @@ const styles = {
           </li>
           <li>
             <a
-              class="section-link"
+              :class="styles.sectionItem"
               href="https://www.airtel.in/b2b/"
               target="_blank"
               rel="noopener noreferrer"
@@ -97,7 +94,7 @@ const styles = {
           </li>
           <li>
             <a
-              class="section-link"
+              :class="styles.sectionItem"
               href="https://www.verizon.com/"
               target="_blank"
               rel="noopener noreferrer"
@@ -167,27 +164,7 @@ h1 a:hover::after {
   color: var(--color-amber-500);
 }
 
-.section-link {
-  font-size: large;
-  text-decoration: underline;
-  text-decoration-thickness: 1px;
-  text-underline-offset: 12px;
-  text-decoration-color: var(--color-stone-600);
-}
-
-.section-link:hover {
-  transition: all 0.2s ease-in-out;
-  color: var(--color-stone-300);
-  text-decoration-color: var(--color-stone-100);
-}
-
-.section-link:hover::after {
-  content: " ↗";
-  color: var(--color-amber-500);
-}
-
 .footer-link {
-  font-size: large;
   text-decoration: underline;
   text-decoration-thickness: 1px;
   text-underline-offset: 12px;
@@ -203,10 +180,5 @@ h1 a:hover::after {
 .footer-link:hover::after {
   content: " ↗";
   color: var(--color-amber-500);
-}
-
-.section-link:focus,
-.footer-link:focus {
-  outline: 1px solid theme("colors.stone.400");
 }
 </style>
